3c4d62d075fe929b0081bf11c4d9822d0c8a32e7
Бугаев Андрей Андреевич <aa.bugaev@empl.s-vfu.ru>
2017-03-24 12:00:49 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Component from './Component';
+import './styles.css'
+
+
+function App() {
+	return (
+		<div>
+			<h1>Sadasdsdsadasdsasha </h1>
+		</div>	
+	);
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
